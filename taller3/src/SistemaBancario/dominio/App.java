11c90bb2bf9e5fea8a8753c6421afa7ab2ba8c48
alexi2020 <alexidiaz123alexi@gmail.com>
2020-07-12 17:38:57 -0400

    Taller 3 prueba

---
+++ taller3/src/SistemaBancario/dominio/App.java
@@ -0,0 +1,11 @@
+package SistemaBancario.dominio;
+import ucn.*;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StdOut.println("hello world");
+	}
+
+}
