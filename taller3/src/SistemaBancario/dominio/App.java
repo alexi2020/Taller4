358100d8b888d49ca89046f5b522588075ea8c30
alexi2020 <alexidiaz123alexi@gmail.com>
2020-07-15 21:25:50 -0400

    actualizacion

--- taller3/src/SistemaBancario/dominio/App.java
+++ taller3/src/SistemaBancario/dominio/App.java
@@ -5,7 +5,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StdOut.println("hello world");
+		StdOut.println("hello world again");
 	}
 
 }
